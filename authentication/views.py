b8878f694ac5a9a661432298593245451c7c5997
jewel <jmbengal.mbstu@gmail.com>
2022-04-05 06:11:15 +0600

    user viewsets created

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from authentication.serializers import UserSerializers
+from authentication.models import User
+from rest_framework import viewsets
 # Create your views here.
+
+class UserViewset(viewsets.ModelViewSet):
+    serializer_class = UserSerializers
+    queryset = User.objects.all()
