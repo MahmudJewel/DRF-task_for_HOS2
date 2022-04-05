e777b4edaca6bec735263a59f46030fd5457e4a6
jewel <jmbengal.mbstu@gmail.com>
2022-04-05 14:08:51 +0600

    some permission changes

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from subscribe.views import SubscribeViewset,CompanyViewset,Company_Phone_ListViewset
+from subscribe.views import SubscribeViewset,CompanyViewset,Company_Phone_ListViewset, AllUserViewset
 
 # router 
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
+router.register('all', AllUserViewset, basename='all')
 router.register('subscribe', SubscribeViewset, basename='subscribe')
 router.register('company', CompanyViewset, basename='company')
 router.register('company-phone', Company_Phone_ListViewset, basename='company-phone')
--- subscribe/views.py
+++ subscribe/views.py
@@ -10,13 +10,18 @@ from rest_framework.response import Response
 
 User=get_user_model()
 
-# Create your views here.
 
-# get, put and post order
+# All Users subscribe view by superuser
+class AllUserViewset(viewsets.ModelViewSet):
+	serializer_class = SubscribeSerializer
+	queryset = Subscribe.objects.all()
+	permission_classes=[IsAdminUser]
+
+# Operation on login user
 class SubscribeViewset(viewsets.ModelViewSet):
 	serializer_class = SubscribeSerializer
 	queryset = Subscribe.objects.all()
-	# permission_classes=[IsAuthenticated]
+	permission_classes=[IsAuthenticated]
 	
 	def create(self, request, *args, **kwargs):
 		data=request.data
@@ -26,12 +31,19 @@ class SubscribeViewset(viewsets.ModelViewSet):
 		serializer = SubscribeSerializer(new_obj)
 		return Response(serializer.data)
 
+	def list(self, request, *args, **kwargs):
+		queryset = Subscribe.objects.get(id=request.user.id)
+		serializer = SubscribeSerializer(queryset)
+		return Response(serializer.data)
+
 # company added by admin
 class CompanyViewset(viewsets.ModelViewSet):
 	serializer_class = CompanySerializer
 	queryset = Company.objects.all()
+	permission_classes=[IsAdminUser]
 
 # This will add multiple phone numbers for specific company
 class Company_Phone_ListViewset(viewsets.ModelViewSet):
 	serializer_class = Company_Phone_ListSerializer
 	queryset = Company_Phone_List.objects.all()
+	permission_classes=[IsAdminUser]
