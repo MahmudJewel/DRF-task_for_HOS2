0a675f41be82eab40f47847ff6907271a8d695f6
jewel <jmbengal.mbstu@gmail.com>
2022-04-05 06:14:35 +0600

    tables are shown on admin site

--- subscribe/admin.py
+++ subscribe/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from subscribe.models import Subscribe, Company, Phone_number, Company_Phone_List
 # Register your models here.
+lst = [Subscribe, Company, Phone_number, Company_Phone_List]
+admin.site.register(lst)
